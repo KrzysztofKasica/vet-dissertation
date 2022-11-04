b64b04da63dc2c6efdaf4d9decd2df495a3ca3cb
KrzysztofKasica <kasicakrzysztof99@gmail.com>
2022-11-04 12:34:06 +0100

    added editpet and getpetbyuser

--- server/src/controllers/pet.ts
+++ server/src/controllers/pet.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { Pet } from "../entities/Pet";
-import { isAuth } from "../middleware/isAuth";
+import { isAuth } from "../isAuth";
 import { dataSourceConn } from "../app-data-source";
 import { Client } from "../entities/Client";
 import { Species } from "../entities/Species";
@@ -37,6 +37,47 @@ export const createPet = async (req: Request, res: Response) => {
         } else {
                 res.status(400).send('No matching species');
         }
+    } else {
+        res.status(400).send('Not Authenticated');
+    }
+}
+
+export const editPet = async (req: Request, res: Response) => {
+    if (isAuth(req)) {
+        const myPet = await petRepository.findOneBy({id: req.body.data.id});
+        if (myPet) {
+            myPet.name = req.body.data.name
+            const mySpecies = await dataSourceConn.manager.findOneBy(Species, {
+                name: req.body.data.species,
+            });
+            if (mySpecies) {
+                myPet.species = mySpecies;
+                myPet.dateOfBirth = new Date(req.body.data.date);
+                try {
+                    await dataSourceConn.manager.save(myPet);
+                    res.status(200).send('Pet updated');
+                }
+                catch (error){
+                    console.log(error);
+                    res.status(500).send('Saving error');
+                }
+            } else {
+                res.status(400).send('No matching species');
+            }
+        } else {
+            res.status(400).send('No matching pet');
+        }
+    } else {
+        res.status(400).send('Not Authenticated');
+    }
+}
+
+export const getPetsByUser = async (req: Request, res: Response) => {
+    if (isAuth(req)) {
+        const myPets = await petRepository.createQueryBuilder("pet")
+        .where("pet.clientId = :id", { id: req.session.clientId })
+        .getMany()
+        res.status(200).send(myPets)
     } else {
         res.status(400).send('Not Authenticated');
     }
--- server/src/routers/pet.ts
+++ server/src/routers/pet.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { createPet } from "../controllers/pet";
+import { createPet, editPet, getPetsByUser } from "../controllers/pet";
 
 export const petRouter = Router();
 
 petRouter.post('/createpet', createPet);
+
+petRouter.patch('/editpet', editPet);
+
+petRouter.get('/getpets', getPetsByUser);
