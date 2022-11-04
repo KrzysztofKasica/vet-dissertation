b4e9539fa91616f680d91219f0b1434034456781
KrzysztofKasica <kasicakrzysztof99@gmail.com>
2022-11-04 19:57:22 +0100

    tabulation on register

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,14 +3,14 @@ import { FormikRegister } from "../components/FormikRegister";
 import NavBar from "../components/NavBar";
 
 const Register = () => (
-        <Flex direction={"column"} justifyContent='center' alignContent={'center'}>
-            <NavBar/>
-            <Center>
-                <Flex maxW='40%'>
-                    <FormikRegister/>
-                </Flex>
-            </Center>
-        </Flex>
+    <Flex direction={"column"} justifyContent='center' alignContent={'center'}>
+        <NavBar/>
+        <Center>
+            <Flex maxW='40%'>
+                <FormikRegister/>
+            </Flex>
+        </Center>
+    </Flex>
 )
 
 export default Register;
